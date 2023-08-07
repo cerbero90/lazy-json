f5700a152d45135a35657bcc411ca385b1a5d33d
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2023-08-07 20:21:09 +0200

    Wrap thrown exceptions

--- src/Exceptions/LazyJsonException.php
+++ src/Exceptions/LazyJsonException.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace Cerbero\LazyJson\Exceptions;
 
 use Exception;
+use Throwable;
 
 final class LazyJsonException extends Exception
 {
-    //
+    public function __construct(public readonly Throwable $exception)
+    {
+        parent::__construct($exception->getMessage());
+    }
 }
